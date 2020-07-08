f0835b9b42519555b2e08422269879a59cf92962
Tobias Kaiser <67737793+mimblgrimpf@users.noreply.github.com>
2020-07-08 13:58:13 +0200

    Added thrust visualization
    Fly the rocket with WASD and QE

--- PandaRocket.py
+++ PandaRocket.py
@@ -2,10 +2,13 @@ import sys
 import direct.directbase.DirectStart
 import math
 from RocketEngine import RocketEngine as RE
+from PID import PID
 
+from direct.showbase.ShowBase import ShowBase
 from direct.showbase.DirectObject import DirectObject
 from direct.showbase.InputStateGlobal import inputState
 from direct.gui.OnscreenText import OnscreenText
+from direct.directtools.DirectGeometry import LineNodePath
 
 from panda3d.core import AmbientLight
 from panda3d.core import DirectionalLight
@@ -17,6 +20,7 @@ from panda3d.core import BitMask32
 from panda3d.core import TextNode
 from panda3d.core import LQuaternionf
 from panda3d.core import LPoint3f
+from panda3d.core import VBase4
 
 from panda3d.bullet import BulletWorld
 from panda3d.bullet import BulletPlaneShape
@@ -25,6 +29,7 @@ from panda3d.bullet import BulletCylinderShape
 from panda3d.bullet import BulletConeShape
 from panda3d.bullet import BulletRigidBodyNode
 from panda3d.bullet import BulletConeTwistConstraint
+from panda3d.bullet import BulletSliderConstraint
 from panda3d.bullet import BulletDebugNode
 from panda3d.bullet import ZUp
 from panda3d.bullet import XUp
@@ -32,17 +37,27 @@ from panda3d.bullet import XUp
 from scipy.spatial.transform import Rotation as rot
 
 
-class Simulation(DirectObject):
+class Simulation(ShowBase):
     scale = 10
+    fuelPID = PID(10,0.5,10,-100,100)
+    EMPTY = 0
+    
+    pitch = 0
+    yaw = 0
       
-    R = RE
+    R = RE(200*9.806,250*9.806,80,0.4)
     throttle = 0.0
-     
+    fuelmass_full = 5
       
     
     def __init__(self):
+        
+        #ShowBase.__init__(self)
         base.setBackgroundColor(0.1, 0.1, 0.8, 1)
         base.setFrameRateMeter(True)
+        
+        
+        self.fuelmass = self.fuelmass_full
     
         base.cam.setPos(-8*self.scale, -8*self.scale, 4*self.scale)
         base.cam.lookAt(0, 0, 1*self.scale)
@@ -114,54 +129,56 @@ class Simulation(DirectObject):
       # ____TASK___
     
     def processInput(self, dt):
-        force = Vec3(0, 0, 0)
         throttleChange = 0.0
-        #torque = Vec3(0, 0, 0)
-    
-        if inputState.isSet('forward'): force.setX(1)
-        if inputState.isSet('reverse'): force.setX(-1)
-        if inputState.isSet('left'):    force.setY(-1)
-        if inputState.isSet('right'):   force.setY( 1)
+        
+        if inputState.isSet('forward'): self.gimbalY = 20
+        if inputState.isSet('reverse'): self.gimbalY = -20
+        if inputState.isSet('left'):    self.gimbalX = 20
+        if inputState.isSet('right'):   self.gimbalX = -20
         if inputState.isSet('turnLeft'):  throttleChange=-1.0
         if inputState.isSet('turnRight'): throttleChange=1.0
-    
-        force *= -20.0/180.0*math.pi
+        
         self.throttle += throttleChange/100.0
         self.throttle = min(max(self.throttle,0),1)
-        thrust=Vec3(0,0,self.throttle*50)
-        #quatGimbal = self.rocketNozzle.getTransform(self.worldNP).getQuat()
         
-        #self.rocketNozzle.node().applyForce(quatGimbal.xform(thrust),LPoint3f(0,0,0))
         
-        #torque *= 10.0
-    
-        #force = render.getRelativeVector(self.rocketNP, force)
-        #torque = render.getRelativeVector(self.rocketNP, torque)
-    
-        #self.rocketNozzle.node().setActive(True)
-        self.rocketNP.node().setActive(True)
-        #self.rocketNozzle.node().applyCentralForce(force)
-        #self.rocketNozzle.node().applyTorque(torque)
-        force = rot.from_euler('zyx',force).as_quat()
-        
-    
-        #self.cone.setMotorTarget(LQuaternionf(force[0],force[1],force[2],force[3]))
 
     def update(self, task):
         dt = globalClock.getDt()
+        
+        pos = self.rocketNP.getPos()
+        quat = self.rocketNP.getTransform().getQuat()
+        
+        self.gimbalX = 0
+        self.gimbalY = 0
     
         self.processInput(dt)
-        #self.world.doPhysics(dt)
+        
+        thrust,mdot =  self.R.setThrottle(self.throttle)
+        self.updateRocket(mdot,dt)
+        
+        quat = self.rocketNP.getTransform().getQuat()
+        quatGimbal = TransformState.makeHpr(Vec3(0,self.gimbalY,self.gimbalX)).getQuat()
+        thrust = quat.xform(quatGimbal.xform(Vec3(thrust[0],thrust[1],thrust[2])))
+        
+        self.npThrustForce.reset()
+        self.npThrustForce.drawArrow2d(Vec3(0,0,-1*self.scale),Vec3(0,0,-1*self.scale)-(thrust)/10, 45, 2)
+        self.npThrustForce.create()
+        
+        self.rocketNP.node().applyForce(thrust,quat.xform(Vec3(0,0,-1*self.scale)))
+        self.rocketNP.node().setActive(True)
+        self.fuelNP.node().setActive(True)
+        
         self.world.doPhysics(dt, 5, 1.0/180.0)
         
-        #yawpitchroll = rot.from_quat(self.rocketNozzle.getTransform(self.rocketNP).getQuat())
-        #yawpitchroll = yawpitchroll.as_euler('zyx',degrees=True)
-         
-        #self.ostData.destroy()
         telemetry = []
-        #telemetry.append('Nozzle Position:\n Yaw: {}\n Pitch: {}\n Roll: {}'.format(
-            #int(yawpitchroll[0]),int(yawpitchroll[1]),int(yawpitchroll[2])))
-        telemetry.append('\nThrottle: {}'.format(self.throttle))
+        
+        telemetry.append('Throttle: {}%'.format(int(self.throttle*100)))
+        telemetry.append('Fuel: {}%'.format(int(self.fuelmass/self.fuelmass_full*100.0)))
+        telemetry.append('Gimbal: {}'.format(int(self.gimbalX))+',{}'.format(int(self.gimbalY)))
+        #telemetry.append('[Debug]FuelPos: {}'.format(self.fuelSlider.getLinearPos()))
+        
+        
         
         self.ostData.setText('\n'.join(telemetry))
         
@@ -194,7 +211,7 @@ class Simulation(DirectObject):
         #self.debugNP.showBounds()
     
         self.world = BulletWorld()
-        self.world.setGravity(Vec3(0, 0, -9.81))
+        self.world.setGravity(Vec3(0, 0,-9.81))
         self.world.setDebugNode(self.debugNP.node())
     
         # Ground (static)
@@ -207,14 +224,17 @@ class Simulation(DirectObject):
     
         self.world.attachRigidBody(self.groundNP.node())
     
+    
+    
         #Rocket
         shape = BulletCylinderShape(0.2*self.scale, 2*self.scale, ZUp)
     
         self.rocketNP = self.worldNP.attachNewNode(BulletRigidBodyNode('Cylinder'))
-        self.rocketNP.node().setMass(3.0)
+        self.rocketNP.node().setMass(1)
         self.rocketNP.node().addShape(shape)
         self.rocketNP.setPos(0, 0, 2*self.scale)
         self.rocketNP.setCollideMask(BitMask32.allOn())
+        #self.rocketNP.node().setCollisionResponse(0)
     
         self.world.attachRigidBody(self.rocketNP.node())
     
@@ -228,65 +248,52 @@ class Simulation(DirectObject):
         
         #Fuel
         shape = BulletCylinderShape(0.15*self.scale, 0.1*self.scale, ZUp)
-        self.fuel  = self.worldNP.attachNewNode(BulletRigidBodyNode('Cone'))
-        self.fuel.node().setMass(1)
-        self.fuel.node().addShape(shape)
-        self.fuel.setPos(0,0,2*self.scale)
-        self.fuel.setCollideMask(BitMask32.allOn())
+        self.fuelNP  = self.worldNP.attachNewNode(BulletRigidBodyNode('Cone'))
+        self.fuelNP.node().setMass(self.fuelmass)
+        self.fuelNP.node().addShape(shape)
+        self.fuelNP.setPos(0,0,2*self.scale)
+        self.fuelNP.setCollideMask(BitMask32.allOn())
+        self.fuelNP.node().setCollisionResponse(0)
     
-        self.rocketNP.node().attachRigidBody(self.fuel.node())
+        self.world.attachRigidBody(self.fuelNP.node())
         
-        frameA = TransformState.makePosHpr(Point3(0, 0, 0*self.scale), Vec3(0, 0, 0))
-        frameB = TransformState.makePosHpr(Point3(0, 0, 0*self.scale), Vec3(0, 0, 0))
+        frameA = TransformState.makePosHpr(Point3(0, 0, 0*self.scale), Vec3(0, 0, 90))
+        frameB = TransformState.makePosHpr(Point3(0, 0, 0*self.scale), Vec3(0, 0, 90))
         
-        self.fuelSlider = BulletConeTwistConstraint(self.rocketNP.node(), self.fuel.node(), frameA, frameB)
-        self.fuelSlider.enableMotor(1)
+        self.fuelSlider = BulletSliderConstraint(self.rocketNP.node(), self.fuelNP.node(), frameA, frameB,1)
+        self.fuelSlider.setPoweredLinearMotor(1)
+        self.fuelSlider.setTargetLinearMotorVelocity(0)
+        self.fuelSlider.setMaxLinearMotorForce(1000)
         #self.cone.setMaxMotorImpulse(2)
         #self.cone.setDamping(1000)
         self.fuelSlider.setDebugDrawSize(2.0)
-        self.rocketNP.attachConstraint(self.fuelSlider)
+        self.world.attachConstraint(self.fuelSlider)
         
-        """
         
-        self.rocketNozzle  = self.worldNP.attachNewNode(BulletRigidBodyNode('Cone'))
-        self.rocketNozzle.node().setMass(1)
-        self.rocketNozzle.node().addShape(shape)
-        self.rocketNozzle.setPos(0,0,0.8*self.scale)
-        self.rocketNozzle.setCollideMask(BitMask32.allOn())
-    
-        self.world.attachRigidBody(self.rocketNozzle.node())
+        self.npThrustForce = LineNodePath(self.rocketNP, 'Thrust', thickness=4, colorVec=VBase4(1, 0.5, 0, 1))
         
-        frameA = TransformState.makePosHpr(Point3(0, 0, -1*self.scale), Vec3(0, 0, 90))
-        frameB = TransformState.makePosHpr(Point3(0, 0, 0.2*self.scale), Vec3(0, 0, 90))
-    
-        self.cone = BulletConeTwistConstraint(self.rocketNP.node(), self.rocketNozzle.node(), frameA, frameB)
-        self.cone.enableMotor(1)
-        #self.cone.setMaxMotorImpulse(2)
-        #self.cone.setDamping(1000)
-        self.cone.setDebugDrawSize(2.0)
-        self.cone.setLimit(20, 20, 0, softness=1.0, bias=1.0, relaxation=10.0)
-        self.world.attachConstraint(self.cone)
-        """
-        """# Box (dynamic)
-        shape = BulletBoxShape(Vec3(0.5, 0.5, 0.5))
-    
-        self.boxNP = self.worldNP.attachNewNode(BulletRigidBodyNode('Box'))
-        self.boxNP.node().setMass(1.0)
-        self.boxNP.node().addShape(shape)
-        self.boxNP.setPos(0, 0, 2)
-        #self.boxNP.setScale(2, 1, 0.5)
-        self.boxNP.setCollideMask(BitMask32.allOn())
-        #self.boxNP.node().setDeactivationEnabled(False)
-    
-        self.world.attachRigidBody(self.boxNP.node())"""
-        """
-        visualNP = loader.loadModel('bullet-samples/models/box.egg')
-        visualNP.clearModelNodes()
-        visualNP.reparentTo(self.rocketNP)
-        """
-        # Bullet nodes should survive a flatten operation!
-        #self.worldNP.flattenStrong()
-        #render.ls()
+    def updateRocket(self,mdot,dt):
+        
+        #Fuel Update
+        self.fuelmass = self.fuelmass - dt*mdot
+        if self.fuelmass <= 0:
+            self.EMPTY = 1
+        fuel_percent = self.fuelmass/self.fuelmass_full
+        self.fuelNP.node().setMass(self.fuelmass)
+        fuelHeight = 2*self.scale*fuel_percent
+        I1 = 1/2*self.fuelmass*0.15*self.scale*0.15*self.scale
+        I2 = 1/4*self.fuelmass*0.15*0.15*self.scale*self.scale+1/12*self.fuelmass*fuelHeight*fuelHeight
+        self.fuelNP.node().setInertia(Vec3(I2,I2,I1))
+        
+        #Shift fuel along slider constraint
+        fuelTargetPos = self.scale-fuelHeight/2
+        fuelPos = self.fuelSlider.getLinearPos()
+        fuelShift = self.fuelPID.control(fuelPos,0,fuelTargetPos)
+        self.fuelSlider.setTargetLinearMotorVelocity(fuelShift)
+        
+        #print(I1,I2)
+        
+        
 
 simulation = Simulation()
 base.run()
