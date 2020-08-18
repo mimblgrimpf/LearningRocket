6df556e25a63dd38a04ce5e4f233567004e6cc19
Tobias Kaiser <67737793+mimblgrimpf@users.noreply.github.com>
2020-08-18 19:54:03 +0200

    Altitude hold

--- LearningRocket.py
+++ LearningRocket.py
@@ -27,28 +27,35 @@ class LearningRocket(gym.Env):
         # Define action and observation space
         # They must be gym.spaces objects
         # Example when using discrete actions:
-        self.action_space = spaces.Box(low=np.array([0.1, -10, -10]), high=np.array([1, 10, 10]), dtype=np.double)
-        #self.action_space = spaces.Box(low=np.array([0.1]), high=np.array([1]), dtype=np.double)
+        #self.action_space = spaces.Box(low=np.array([0.1, -10, -10]), high=np.array([1, 10, 10]), dtype=np.double)
+        self.action_space = spaces.Box(low=np.array([0.1]), high=np.array([1]), dtype=np.double)
         # Example for using image as input:
         """self.observation_space = spaces.Box(
             low=np.array([-400, -400, 0, -100, -100, -100, -180, -180, -180, -2, -2, -2, 0]),
             high=np.array([400, 400, 1000, 100, 100, 100, 180, 180, 180, 2, 2, 2, 1]),
             dtype=np.double)"""
-
+        """
         self.observation_space = spaces.Box(
             low=np.array([-400, -400, 0, -180, -180, -180]),
             high=np.array([400, 400, 1000, 180, 180, 180]),
-            dtype=np.double)
+            dtype=np.double)"""
+
         """
         self.observation_space = spaces.Box(
-            low=np.array([0,-100]),
-            high=np.array([1000,100]),
+            low=np.array([-400,-400,-100,-100,-180,-180,-2,-2]),
+            high=np.array([400,400,100,100,180,180,2,2]),
             dtype=np.double)"""
+
+        self.observation_space = spaces.Box(
+            low=np.array([-0, -100]),
+            high=np.array([1000, 100]),
+            dtype=np.double)
+
 
         # base.run()
 
     def step(self, action):
-        self.sim.control(action[0], action[1], action[2])
+        self.sim.control(action[0])#, action[1], action[2])
         # print("In LR: {}".format(action[0]))
         # self.sim.update()
 
@@ -56,19 +63,19 @@ class LearningRocket(gym.Env):
         """observation = np.array([pos[0], pos[1], pos[2], vel[0], vel[1], vel[2], Roll, Pitch, Yaw, rotVel[0], rotVel[1],
                                 rotVel[2], fuel])"""
 
-        observation = np.array([pos[0], pos[1], pos[2], Roll, Pitch, Yaw])
-        #observation = np.array([pos[2],vel[2]])
+        #observation = np.array([pos[0], pos[1], vel[0], vel[1], Pitch, Yaw, rotVel[0], rotVel[1]])
+        observation = np.array([pos[2],vel[2]])
         done = LANDED
 
         if LANDED is True:
 
             #reward = -100.0 * (mag(pos) + 10.0 * mag(vel) + abs(Pitch) + abs(Yaw) + mag(rotVel)) / 100000.0
-            reward = -(abs(pos.getX()) + abs(pos.getY())) / 400.0
-            #reward = -abs(pos.getZ()-500)/500#+1
+            #reward = -(abs(pos.getX()) + abs(pos.getY())) / 400.0 /2.0
+            reward = -abs(pos.getZ()-500)/500#+1
         else:
             # reward = -0.01 * (abs(Pitch) + abs(Yaw) + mag(rotVel) + abs(pos.getX()) + abs(pos.getY())) / 100000.0
-            reward = -(abs(pos.getX()) + abs(pos.getY())) / 400.0
-            #reward = -abs(pos.getZ()-500)/500#+1
+            #reward = -(abs(pos.getX()) + abs(pos.getY())) / 400.0 /2.0
+            reward = -abs(pos.getZ()-500)/500#+1
             # reward = 0
         info = {
             "A": "B"
@@ -78,11 +85,15 @@ class LearningRocket(gym.Env):
     def reset(self):
         self.sim.doReset()
         pos, vel, Roll, Pitch, Yaw, rotVel, fuel, EMPTY, LANDED = self.sim.observe()
-        observation = np.array([pos[0], pos[1], pos[2], Roll, Pitch, Yaw])
+        #observation = np.array([pos[0], pos[1], pos[2], Roll, Pitch, Yaw])
+        #observation = np.array([pos[0], pos[1], vel[0], vel[1], Pitch, Yaw, rotVel[0], rotVel[1]])
+        observation = np.array([pos[2], vel[2]])
+
         return observation
 
     def render(self, visualize=False):
-        self.sim.setVisualization(visualize)
+        #self.sim.setVisualization(visualize)
+        ...
 
     def close(self):
         ...
